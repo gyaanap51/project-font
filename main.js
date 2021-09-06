f5f74a3fc5adbb3a6465d9e7bf8951e88893d9b3
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-09-06 14:25:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(640,480);
+    video = createCapture(VIDEO);
+    canvas.position(650,100);
+    video.size(640,480);
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+   
+}
+function draw(){
+    background('#2AD5AF');
+}
+function modelLoaded(){
+    console.log("PoseNet Is Initialized!");
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
